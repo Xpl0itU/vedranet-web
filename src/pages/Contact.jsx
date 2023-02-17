0a36bc69e64365647ada532dd655e2472a3e635d
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-02-17 11:14:04 +0100

    Add Contact page (finish this)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,21 @@
+import { Description } from "@mui/icons-material";
+import "./Contact.css"
 function Contact() {
   return (
     <>
-      <p>Contact page</p>
+      <p id="p1">Contacto</p>
+      <div id="Description"><p>Tenemos varias posibilidades de contacto</p>
+      <ul id="list1">
+        <li><a href="">Facebook</a></li>
+        <li><a href="">Twitter</a></li>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">E-mail</a></li> 
+      </ul>    
+      </div>
+      <img id="img1" src="facebook.png"></img>
+      <img id="img2" src="twitter.png"></img>
+      <img id="img3" src="instagram.jpg"></img>
+      <img id="img4" src="gmail.jpeg"></img>
     </>
   );
 }
